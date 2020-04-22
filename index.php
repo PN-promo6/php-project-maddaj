2219ca6a04f1875fb61459f8f03aa796ea17f57a
maddaj <j.maddalena2@gmail.com>
2020-04-22 20:07:39 +0200

    Change design

--- index.php
+++ index.php
@@ -18,92 +18,87 @@
 </head>
 
 <body>
-    <div id="scroll-animate">
-        <div id="scroll-animate-main">
-            <!-- NAV -->
-            <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-                <a class="navbar-brand" href="#">🎨</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
-                    <ul class="navbar-nav">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                                Catégories
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Contact</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-
-            <div class="wrapper-parallax">
-                <!-- HEADER -->
-                <header>
-                    <h1>À chaque siècle son art, à l’art sa liberté</h1>
-                </header>
-
-                <!-- MAIN -->
-                <section class="content">
-
-                    <div class="row">
-                        <!-- filtres de recherche -->
-                        <!-- <div class="col-md-3 border-right">
-                            
-                        </div> -->
-                        <div class="col-12">
-                            <div class="row d-flex justify-content-center">
-                                <div class="col-3">
-                                    <div class="card">
-                                        <img src="https://images.pexels.com/photos/2236960/pexels-photo-2236960.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <p>@username</p>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Voir plus d'oeuvres de cet artiste</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-3">
-                                    <div class="card">
-                                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTNuoY4yPpcE46Zqp8R6dM37FU0k9VIDSdsg1rZoYgxLEyUYSxY&usqp=CAU" class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <p>@username</p>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Voir plus d'oeuvres de cet artiste</a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-3">
-                                    <div class="card">
-                                        <img src="https://images-na.ssl-images-amazon.com/images/I/91Oo7MLeVLL._AC_SL1500_.jpg" class="card-img-top" alt="...">
-                                        <div class="card-body">
-                                            <p>@username</p>
-                                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                            <a href="#" class="btn btn-primary">Voir plus d'oeuvres de cet artiste</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+            <a class="navbar-brand" href="#">🎨</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">
+                            Catégories
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="fullpage duotone">
+            <h1>"La créativité est contagieuse, faites la tourner."</h1>
+            <p>Albert Einstein</p>
+        </div>
+    </header>
+    <!-- MAIN -->
+    <div class="container main-container">
+        <h2>Nouveautés</h2>
+        <div class="row">
+            <div class="col-md-9 d-flex mx-auto">
+                <article class="card card--1">
+                    <div class="card__img"></div>
+                    <a href="#" class="card_link">
+                        <div class="card__img--hover"></div>
+                    </a>
+                    <div class="card__info">
+                        <span class="card__category">Catégories</span>
+                        <h3 class="card__title">Text</h3>
+                        <span class="card__by">by <a href="#" class="card__author" title="author">Username</a></span>
                     </div>
-
-                </section>
-
-                <!-- FOOTER -->
-                <footer>
-                    <h1>Footer</h1>
-                </footer>
+                </article>
+                <article class="card card--2">
+                    <div class="card__img"></div>
+                    <a href="#" class="card_link">
+                        <div class="card__img--hover"></div>
+                    </a>
+                    <div class="card__info">
+                        <span class="card__category">Catégories</span>
+                        <h3 class="card__title">Text</h3>
+                        <span class="card__by">by <a href="#" class="card__author" title="author">Username</a></span>
+                    </div>
+                </article>
+                <article class="card card--3">
+                    <div class="card__img"></div>
+                    <a href="#" class="card_link">
+                        <div class="card__img--hover"></div>
+                    </a>
+                    <div class="card__info">
+                        <span class="card__category">Catégories</span>
+                        <h3 class="card__title">Text</h3>
+                        <span class="card__by">by <a href="#" class="card__author" title="author">Username</a></span>
+                    </div>
+                </article>
 
             </div>
         </div>
     </div>
 
+    <!-- FOOTER -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+
+                </div>
+
+            </div>
+        </div>
+    </footer>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
