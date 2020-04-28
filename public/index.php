c4f9550becd7f5abac8c6b3ccec89e3d23688d85
maddaj <j.maddalena2@gmail.com>
2020-04-28 11:40:31 +0200

    Update card style

--- public/index.php
+++ public/index.php
@@ -7,17 +7,18 @@ use Entity\User;
 
 $usr1 = new User();
 $usr1->id = 1;
-$usr1->nickname = "toto";
+$usr1->nickname = "Username";
 $usr1->password = "eeeee";
 $usr1->email = "toto@gmail.com";
 $usr1->website = "http://www.toto.com";
 
 $art1 = new Article();
 $art1->id = 1;
+$art1->text = "lorem ipsum";
 $art1->main_color = "#cccccc";
 $art1->size = "50x60";
-$art1->url_image = "https://desenio.fr/bilder/artiklar/zoom/pre0004_5.jpg";
-$art1->category = "tableau";
+$art1->url_image = "https://images.pexels.com/photos/2236960/pexels-photo-2236960.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
+$art1->category = "graff";
 $art1->user = $usr1;
 
 $articles = array($art1, $art1, $art1, $art1, $art1, $art1, $art1, $art1, $art1);
@@ -37,8 +38,7 @@ $articles = array($art1, $art1, $art1, $art1, $art1, $art1, $art1, $art1, $art1)
     <!-- FONTS -->
     <link href="https://fonts.googleapis.com/css2?family=Lexend+Giga&display=swap" rel="stylesheet">
     <!-- CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css?toto=<?php echo time() ?>">
 
 </head>
@@ -49,8 +49,7 @@ $articles = array($art1, $art1, $art1, $art1, $art1, $art1, $art1, $art1, $art1)
         <div class="fullpage duotone">
             <nav class="navbar navbar-expand-lg navbar-default fixed-top">
                 <a class="navbar-brand" href="#">🎨</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNav">
@@ -74,26 +73,27 @@ $articles = array($art1, $art1, $art1, $art1, $art1, $art1, $art1, $art1, $art1)
     </header>
     <!-- MAIN -->
     <div class="container main-container">
-        <h2>Nouveautés</h2>
-        <div class="row">
+        <h2 class="d-flex justify-content-center">Nouveautés</h2>
+        <div class="row justify-content-around card-row">
             <?php
             $i = 0;
+            // Take item from a table
             foreach ($articles as $oneArticle) {
-                if ($i % 3 == 0) {
-                    echo '</div><div class="row">';
+                if ($i % 3 == 0 && $i > 0) {
+                    echo '</div><div class="row justify-content-around card-row">';
                 }
             ?>
-            <article class="col-md-4 card card--1">
-                <div class="card__img"></div>
-                <a href="#" class="card_link">
-                    <div class="card__img--hover"></div>
-                </a>
-                <div class="card__info">
-                    <span class="card__category">Catégories</span>
-                    <h3 class="card__title">Text</h3>
-                    <span class="card__by">by <a href="#" class="card__author" title="author">Username</a></span>
-                </div>
-            </article>
+                <article class="col-md-3 card card--1 px-0">
+                    <img class="card__img" src="<?php echo $oneArticle->url_image ?>" alt="">
+                    <a href="#" class="card_link">
+                        <img class="card__img--hover" src="<?php echo $oneArticle->url_image ?>" alt="">
+                    </a>
+                    <div class="card__info">
+                        <span class="card__category"><?php echo $oneArticle->category ?></span>
+                        <h3 class="card__title"><?php echo $oneArticle->text ?></h3>
+                        <span class="card__by">by <a href="#" class="card__author" title="author"><?php echo $oneArticle->user->nickname ?></a></span>
+                    </div>
+                </article>
             <?php
                 $i++;
             }
@@ -112,14 +112,11 @@ $articles = array($art1, $art1, $art1, $art1, $art1, $art1, $art1, $art1, $art1)
             </div>
         </div>
     </footer>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
     </script>
 
 </body>
