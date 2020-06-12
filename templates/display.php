94c2940bf830c24a21e1b84283d4eab1958edb60
maddaj <j.maddalena2@gmail.com>
2020-06-12 17:17:39 +0200

    Change search field

--- templates/display.php
+++ templates/display.php
@@ -19,24 +19,23 @@
         foreach ($articles as $oneArticle) {
         ?>
             <div class="col-12 col-md-4 col-lg-3 justify-content-around my-5 justify-content-around">
-                <div class="dribble-card">
-                    <img class="dribble-card-image" src="<?php echo $oneArticle->url_image ?>" alt="">
+                <div class="dribble-card" style="background-image: url('<?php echo $oneArticle->url_image ?>')">
                     <div class="dribble-hover">
                         <p class="dribble-hover-title"><?php echo $oneArticle->category ?></p>
                         <p class="dribble-hover-title"><?php echo $oneArticle->text ?></p>
                     </div>
-                    <div class="dribble-meta">
-                        <div class="dribble-author">
-                            <img src="https://cdn.dribbble.com/users/63407/avatars/original/avatar.png" alt="author" />
-                            <a href="?search=@<?php echo $oneArticle->user->nickname ?>" class="card__author" title="author">@<?php echo $oneArticle->user->nickname ?></a>
-                        </div>
-                    </div>
+                </div>
+                <div class="dribble-meta">
+                    <div class="dribble-author"">
+                            <img src=" https://cdn.dribbble.com/users/63407/avatars/original/avatar.png" alt="author" />
+                    <a href="?search=@<?php echo $oneArticle->user->nickname ?>" class="card__author" title="author">@<?php echo $oneArticle->user->nickname ?></a>
                 </div>
             </div>
-        <?php
-        }
-        ?>
     </div>
+<?php
+        }
+?>
+</div>
 </div>
 
 {% include 'footer.php' %}
--- templates/header.php
+++ templates/header.php
@@ -10,8 +10,10 @@
     <!-- CSS -->
 
     <link rel="icon" type="image/png" href="img/icon.png">
-    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
     <link rel="stylesheet" href="css/style.css">
 
 </head>
@@ -19,12 +21,12 @@
 <body>
     <header>
 
-        <nav class="navbar navbar-expand-lg navbar-default fixed-top">
-            <a class="navbar-brand" href="#">🎨</a>
+        <nav class="navbar navbar-expand-lg navbar-default fixed-top row">
+            <a class="navbar-brand col-2" href="#">🎨</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="collapse navbar-collapse col-12 col-md-8" id="navbarSupportedContent">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
                         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
@@ -58,11 +60,15 @@
                     <?php
                     }
                     ?>
-                    <form class="form-inline my-2 my-lg-0">
-                        <input class="form-control mr-sm-2" type="search" name="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
+
                 </ul>
             </div>
+            <div class="col-2">
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control input-search" type="search" name="search" placeholder="Search" aria-label="Search">
+                    <button class="btn my-2 my-sm-0 search-btn" type="submit"><i class="fas fa-search"></i></button>
+                </form>
+            </div>
+
         </nav>
     </header>
